189920e4bebab735e15cc07d4e4dae99db405b5f
Doyeon Kim <ehdus9809@gmail.com>
2025-07-16 06:24:40 +0900

    [Chore] Lombok 의존성 및 플러그인 설정 추가

--- src/main/java/emory/emoryserver/config/SwaggerConfig.java
+++ src/main/java/emory/emoryserver/config/SwaggerConfig.java
@@ -1,11 +1,8 @@
 package emory.emoryserver.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,25 +11,16 @@ import java.util.List;
 @Configuration
 public class SwaggerConfig {
 
-    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
-
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("EMORY API")
                         .version("v1"))
-                // JWT 인증 스키마 추가
-                .components(new Components()
-                        .addSecuritySchemes(
-                                SECURITY_SCHEME_NAME,
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")
-                        )
-                )
-                // Swagger UI에서 Authorize 버튼 활성화 (JWT 입력 가능)
-                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME));
+                .servers(List.of(
+                        new Server()
+                                .url("https://emory-server-406346608321.asia-northeast3.run.app")
+                                .description("Cloud Run HTTPS endpoint")
+                ));
     }
 }
--- src/main/java/emory/emoryserver/global/config/ForwardedHeaderConfig.java
+++ src/main/java/emory/emoryserver/global/config/ForwardedHeaderConfig.java
@@ -2,15 +2,12 @@ package emory.emoryserver.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @Configuration
 public class ForwardedHeaderConfig {
 
-    // @Order(0) → 필터 체인에서 가장 먼저 실행되도록 보장
     @Bean
-    @Order(0)
     public ForwardedHeaderFilter forwardedHeaderFilter() {
         return new ForwardedHeaderFilter();
     }
