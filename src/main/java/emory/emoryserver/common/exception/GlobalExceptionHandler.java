8ffe06d61b9a55d12158d80ed9e834bba79a1362
dahyun1060 <dahyun1060@naver.com>
2025-08-04 00:51:30 +0900

    [Feat] common 패키지 초기 세팅

---
+++ src/main/java/emory/emoryserver/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package emory.emoryserver.common.exception;
+
+import emory.emoryserver.common.dto.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiResponse<Void>> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.badRequest()
+                .body(ApiResponse.error(e.getMessage()));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ApiResponse<Void>> handleRuntimeException(RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.error("서버 오류가 발생했습니다."));
+    }
+}
