c0553cecc233090d1f09b0e05e600f5eb154ec83
Doyeon Kim <ehdus9809@gmail.com>
2025-08-13 18:26:51 +0900

    [feat] Aidiary 모델에 userId와 dateOfDay 기반 복합 인덱스 추가

--- src/main/java/emory/emoryserver/aidiary/dto/DiaryGenerateRequestDto.java
+++ src/main/java/emory/emoryserver/aidiary/dto/DiaryGenerateRequestDto.java
@@ -3,11 +3,11 @@ package emory.emoryserver.aidiary.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Data
 public class DiaryGenerateRequestDto {
-    private List<String> chatLogs;
     private String sessionId;
-    private String userId;
+    private LocalDate dateOfDay;
 }
--- src/main/java/emory/emoryserver/aidiary/model/AiDiary.java
+++ src/main/java/emory/emoryserver/aidiary/model/AiDiary.java
@@ -3,10 +3,15 @@ package emory.emoryserver.aidiary.model;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -15,11 +20,14 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Document(collection = "ai_diaries") // mongoDB 컬렉션명
+@CompoundIndex(name = "uid_date_idx", def = "{'userId': 1, 'dateOfDay': -1}" )
 public class AiDiary {
     @Id
     private String id;
 
+    @Indexed
     private String sessionId;
+    @Indexed
     private String userId;
 
     private String title;
@@ -28,13 +36,21 @@ public class AiDiary {
     private String mood;
     private String imageId;
 
-    private Integer version;  // ex: 1
-    private String status;    // "DRAFT" | "FINAL"
-    private Boolean editable; // true/false
+    @Builder.Default
+    private Integer version = 1; //v1
+    @Builder.Default
+    private String status = "DRAFT";// "DRAFT" | "FINAL"
+    @Builder.Default
+    private Boolean editable = true; // true/false
 
     private LocalDate dateOfDay;
+
+    @CreatedDate
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 
-    private List<DiaryEdit> history;
+    @Builder.Default
+    private List<DiaryEdit> history = new ArrayList<>();
 }
