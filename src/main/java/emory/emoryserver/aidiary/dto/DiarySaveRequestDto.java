efd2e848e76d945e72e1bd9fc08a059f39c9ade4
Doyeon Kim <ehdus9809@gmail.com>
2025-09-02 19:03:37 +0900

    [refactor] 일기 저장 Dto 필요없는 코드 정리

--- src/main/java/emory/emoryserver/aidiary/dto/DiarySaveRequestDto.java
+++ src/main/java/emory/emoryserver/aidiary/dto/DiarySaveRequestDto.java
@@ -2,15 +2,10 @@ package emory.emoryserver.aidiary.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
-import java.time.LocalDate;
 
-@Getter
-@Setter
+@Data
 public class DiarySaveRequestDto {
     private String diaryId;      // MongoDB ObjectId
-    private String content;      // 수정된 일기 내용
-    private String imageId;     // AI가 생성한 이미지 URl
+   // private String imageId;     // AI가 생성한 이미지 URl
 }
