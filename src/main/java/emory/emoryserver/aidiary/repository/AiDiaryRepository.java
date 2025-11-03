236e412dae84dc345b74a373563a7d757e4ec4be
Doyeon Kim <ehdus9809@gmail.com>
2025-11-03 20:58:13 +0900

    [fix] AiDiaryRepository에 어노테이션 추가

--- src/main/java/emory/emoryserver/aidiary/repository/AiDiaryRepository.java
+++ src/main/java/emory/emoryserver/aidiary/repository/AiDiaryRepository.java
@@ -2,6 +2,8 @@ package emory.emoryserver.aidiary.repository;
 
 import emory.emoryserver.aidiary.model.AiDiary;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AiDiaryRepository extends MongoRepository<AiDiary, String> {
 }
