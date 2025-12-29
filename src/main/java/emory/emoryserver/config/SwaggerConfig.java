97a621f4585f4b7e2fe88af45ba88c0fd765bece
eunground <jeshin@sookmyung.ac.kr>
2025-12-29 23:06:38 +0900

    Feat: bearer 인증

--- src/main/java/emory/emoryserver/config/SwaggerConfig.java
+++ src/main/java/emory/emoryserver/config/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package emory.emoryserver.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,12 +13,33 @@ import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
     public OpenAPI customOpenAPI() {
+        String schemeName = "bearerAuth";
+
         return new OpenAPI()
-                .info(new Info().title("EMORY API").version("v1"))
-                .servers(List.of(new Server()
-                        .url("https://emory-server-406346608321.asia-northeast3.run.app")
-                        .description("Cloud Run HTTPS endpoint")));
+                .info(new Info()
+                        .title("EMORY API")
+                        .version("v1")
+                )
+
+                .servers(List.of(
+                        new Server()
+                                .url("https://emory-server-406346608321.asia-northeast3.run.app")
+                                .description("Cloud Run HTTPS endpoint")
+                ))
+
+                .addSecurityItem(new SecurityRequirement().addList(schemeName))
+
+                .components(new Components()
+                        .addSecuritySchemes(
+                                schemeName,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
     }
 }
