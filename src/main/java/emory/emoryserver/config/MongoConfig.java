b30294c9b382cba3584fbf867c88024b35bdbc51
dahyun1060 <128482804+dahyun1060@users.noreply.github.com>
2026-02-14 12:13:26 +0900

    fix: mongoconfig 수정

--- src/main/java/emory/emoryserver/config/MongoConfig.java
+++ src/main/java/emory/emoryserver/config/MongoConfig.java
@@ -1,15 +1,10 @@
 package emory.emoryserver.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @Configuration
 @EnableMongoAuditing
-public class MongoConfig extends AbstractMongoClientConfiguration {
-
-    @Override
-    protected String getDatabaseName() {
-        return "emory";
-    }
+public class MongoConfig {
+    //
 }
